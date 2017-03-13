7e20aa9bb664828deade25f9cfb7561df24ff915
mojeld <mojelder@gmail.com>
2017-03-13 13:57:14 +0900

    チュートリアルシリーズ2 第8回 作ってみよう Delphiの部

---
+++ marupeke_delphi/Unit1.pas
@@ -0,0 +1,180 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.ScrollBox, FMX.Memo, System.DateUtils,
+  FMX.Objects, System.Generics.Collections, Unit2;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    SpeedButton5: TSpeedButton;
+    SpeedButton6: TSpeedButton;
+    SpeedButton7: TSpeedButton;
+    SpeedButton8: TSpeedButton;
+    SpeedButton9: TSpeedButton;
+    SpeedButton10: TSpeedButton;
+    SpeedButton11: TSpeedButton;
+    SpeedButton12: TSpeedButton;
+    Timer1: TTimer;
+    Line1: TLine;
+    Line2: TLine;
+    Line3: TLine;
+    Line4: TLine;
+    procedure SpeedButton12Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    Marupeke1:  TMarupeke;
+    procedure SpeedButtonsClick(Sender: TObject);
+  public
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);//フォーム作成時
+var
+  i:  Integer;
+begin
+  Marupeke1 := TMarupeke.Create(Timer1);  //TMarupekeインスタンスを作る
+  for i := 0 to 8 do                      //1～9のボタンをTMarupekeにセット
+    Marupeke1.Buttons.Add(
+      TSpeedButton(Self.FindComponent(Format('SpeedButton%d', [i+1]))) );
+  Timer1.Enabled          := False;
+  SpeedButton10.IsPressed := True;
+  Randomize;
+  Marupeke1.game_clear(False, SpeedButton11.IsPressed, SpeedButtonsClick);
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);//フォーム終了時
+begin
+  Marupeke1.DisposeOf;  //TMarupeke捨てる
+end;
+
+
+procedure TForm1.SpeedButton12Click(Sender: TObject);
+begin
+  Marupeke1.game_clear(True, SpeedButton11.IsPressed, SpeedButtonsClick);
+  Marupeke1.start_flg := True;
+end;
+
+procedure TForm1.SpeedButtonsClick(Sender: TObject);
+begin
+  ///三目９マスのボタンのクリックイベント
+  with (Sender as TSpeedButton) do
+  begin
+    if Tag = 0 then
+    begin
+      IsPressed := True;
+      Tag       := player;  ///Unit2の定数の値
+      Text      := '〇';    ///プレーヤは先攻/後攻関係なく〇が入る
+      if Marupeke1.triumph > 1 then
+      begin  ///条件が揃ったらWinを出してゲームクリア
+        ShowMessage('Win');
+        Marupeke1.game_clear(False, SpeedButton11.IsPressed, SpeedButtonsClick);
+      end;
+      Timer1.Enabled  := True;  ///タイマーを使いコンピュータへターンを渡す
+    end;
+  end;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+var
+  random_:    Integer;
+  sp_button_: TSpeedButton;
+  next_:      Boolean;
+  dtStart:    TDateTime;
+  kouho:      Integer;
+  bflg:       Boolean;
+  counts_:    TCounts;
+begin
+  Timer1.Enabled  := False;
+  dtStart := IncSecond(Now, 1); ///タイムアウト用1秒以上経過するとタイムアウト処理
+  next_   := False;
+  kouho := -1;
+  if Marupeke1.start_flg then
+  begin
+    bflg  := False;
+    ///無名メソッドを使い次の手を考えさせます
+    TMarupeke.line_list(procedure (line_: TLines) var i: Integer; begin
+      counts_ := TCounts.Create(0,0);
+      {*********************************************************************}
+      {     TLines配列に〇と×が何個入っているか調べる
+            このコードではプレーヤ側のリーチだけを見ています。(保守的)
+            Question: 自分(コンピュータ)のリーチを調べて自分から勝つコードを
+                      追記修正してみてください。                            }
+      {*********************************************************************}
+      for i:=0 to Length(line_)-1 do
+      begin
+        sp_button_  := TSpeedButton(Self.FindComponent(Format('SpeedButton%d', [line_[i]+1])));
+        if sp_button_.Tag = player then
+          Inc(counts_.count1);
+        if sp_button_.Tag = computer then
+          Inc(counts_.count2);
+
+        if (sp_button_.Tag = 0) and (not bflg) then
+        begin
+          kouho := line_[i];//次の手の候補を選ぶ
+        end;
+      end;
+      if (counts_.count1 >= 2) and (counts_ < TCounts.Create(3,0)) then
+      begin //TLines配列に〇が2つある場合で配列が埋まっていない場合
+        counts_ := TCounts.Create(0,0);
+        bflg    := True;
+      end;
+    end);
+
+    while (not next_)  do
+    begin
+      if kouho >= 0 then  //次の手があればそれを利用し
+        random_ := kouho
+      else                //手が何も無い場合randomで選ぶ
+        random_ := Random(9);
+      Sleep(100);
+      sp_button_  := TSpeedButton(Self.FindComponent(Format('SpeedButton%d', [random_+1])));
+      if sp_button_.Tag = 0 then
+      begin ///ここでコンピュータ側の手を入れます。
+        sp_button_.Tag        := computer;  ///定数
+        sp_button_.Text       := '×';      ///コンピュータ側は先攻/後攻全て×
+        sp_button_.IsPressed  := True;
+        next_ := True;
+      end
+      else
+        kouho := -1;
+
+      if (dtStart < Now) then
+      begin //無限ループしないようにタイムアウトを作る
+        next_ := True;
+        ShowMessage('draw');
+      end;
+    end;
+
+    {*********************************************************************}
+    {     triumphは三目並べれば1以上を返しています。
+          Question: function TMarupeke.triumph: Integer;を変更し
+                    Win/Loseを作ってみてください。                        }
+    {*********************************************************************}
+    if Marupeke1.triumph > 1 then
+    begin //triumphで勝敗がついている場合
+      ShowMessage('Win');
+      Marupeke1.game_clear(False, SpeedButton11.IsPressed, SpeedButtonsClick);
+    end;
+  end;
+end;
+
+end.
---
+++ marupeke_delphi/Unit2.pas
@@ -0,0 +1,167 @@
+﻿unit Unit2;
+
+interface
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, FMX.StdCtrls,
+  FMX.Types,  //TTimer用
+  System.Generics.Collections;
+
+const
+  player    = 1;
+  computer  = 2;
+
+type
+  TCounts = record
+    count1:     Integer;
+    count2:     Integer;
+    constructor Create(c1, c2: Integer); overload;
+    class operator LessThan(a: TCounts; b: TCounts): Boolean;
+  end;
+  TLines = array of Integer;
+  TMarupeke = class(TObject)
+    constructor Create(const tim_: TTimer);
+    destructor Destroy; override;
+  private
+    Fstart_flg:     Boolean;
+    FButtons:   TList<TSpeedButton>;  //1～9までのボタン
+    FMainTimer: TTimer;               //メイン側のタイマー
+    function getButtons: TList<TSpeedButton>;
+  public
+    ///<summary>ボタンを呼出すプロパティ</summary>
+    property  Buttons: TList<TSpeedButton>  read getButtons;
+    procedure game_clear(start, attacked: Boolean; event_: TNotifyEvent);
+    class procedure line_list(proc: TProc<TLines>);
+    function triumph: Integer;
+    function triumph_row(r: TLines): Integer;
+    property  start_flg: Boolean  read Fstart_flg write Fstart_flg;
+  end;
+
+implementation
+
+{ TMarupeke }
+
+constructor TMarupeke.Create(const tim_: TTimer);
+begin
+  inherited Create;
+  Fstart_flg  := False;
+  FButtons    := TList<TSpeedButton>.Create;
+  FMainTimer  := tim_;
+end;
+
+destructor TMarupeke.Destroy;
+begin
+  FButtons.DisposeOf;
+  inherited;
+end;
+
+procedure TMarupeke.game_clear(start, attacked: Boolean; event_: TNotifyEvent);
+var
+  btn_: TSpeedButton;
+begin
+  for btn_ in FButtons do  //FindComponentで1～9までのSpeedButtonを探す
+    with btn_ do
+    begin
+      IsPressed := False;  //プレス
+      OnClick   := event_; //共通クリックイベントの設定
+      Text      := '';
+      Tag       := 0;
+      if start then
+        Enabled := True
+      else
+        Enabled := False;
+    end;
+  FMainTimer.Enabled  := attacked;  //後攻ならCOMのターン
+end;
+
+function TMarupeke.getButtons: TList<TSpeedButton>;
+begin
+  Result  := FButtons;
+end;
+
+class procedure TMarupeke.line_list(proc: TProc<TLines>);
+const
+  triumph_naname: array[0..2, 0..2] of Integer = ((0,4,8),(2,4,6),(0,0,0));
+var
+  i: Integer;
+  i1: Integer;
+  i2: Integer;
+  z:  TLines;
+  zi: Integer;
+begin
+  SetLength(z, 3);
+  for i := 0 to 2 do
+    for i1 := 0 to 2 do
+    begin
+      for i2 := 0 to 2 do
+      begin
+        case i of
+        0:z[i2] := i2 + (i1*3);
+        1:z[i2] := (i2*3) + i1;
+        2: begin
+          z[i2] := triumph_naname[i1,i2];
+        end;
+        end;
+      end;
+      if not ((z[0] = 0) and (z[1] = 0) and (z[2] = 0)) then
+        proc(z);
+    end;
+end;
+
+function TMarupeke.triumph: Integer;
+var
+  answer_:  Integer;
+begin
+  answer_  := 0;
+  line_list(procedure (z: TLines) begin
+    if (z[1] > 0) and (z[2] > 0) then
+      answer_ := triumph_row(z);  //三目揃ったか？
+
+    if answer_ >1 then  //どちらかが三目揃った場合
+    begin
+      Fstart_flg := False;
+      FMainTimer.Enabled  := False;
+    end
+  end);
+  if not Fstart_flg then
+    Result  := 2
+  else
+    Result  := answer_;
+end;
+
+function TMarupeke.triumph_row(r: TLines): Integer;
+var
+  com_count: Integer;
+  iTag, temp_tag:       Integer;
+  answer_: Integer;
+begin
+  answer_   := 0;
+  iTag      := -1;
+  for com_count := 0 to Length(r)-1 do
+  begin
+    temp_tag  := FButtons[r[com_count]].Tag;
+    if not(temp_tag = 0) then
+      if (not(com_count=0) and (temp_tag = iTag)) then
+        Inc(answer_)
+      else
+        iTag  := temp_tag;
+  end;
+  Result  := answer_;
+end;
+
+{ TCounts }
+
+constructor TCounts.Create(c1, c2: Integer);
+begin
+  count1  := c1;
+  count2  := c2;
+end;
+
+class operator TCounts.LessThan(a, b: TCounts): Boolean;
+begin
+  if (a.count1 + a.count2) < (b.count1 + b.count2) then
+    Result  := True
+  else
+    Result  := False;
+end;
+
+end.
